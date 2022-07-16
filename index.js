4ab17b4479ebea1a49018b6137eff2139217c0a9
SinhTNgoc <tranngocsinh2211@gmail.com>
2022-07-16 22:35:56 +0700

    Start server express

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded());
+
+//connect db
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+const PORT = process.env.PORT || 8800;
+app.listen(PORT, () => {
+  connect();
+  console.log(`Server is running on Port ${PORT}`);
+});
